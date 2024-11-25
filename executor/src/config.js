2f45af073ce20226f0505ea6c88b02c900bf7bf8
jaypatel1210 <jd4215124@gmail.com>
2024-11-25 11:23:56 +0530

    chore: change origin with actual ui URL

--- executor/src/config.js
+++ executor/src/config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-  ALLOWED_ORIGINS: '*', // TODO: Change this to the actual domain
+  ALLOWED_ORIGINS: 'https://cloud-ide.jaypatel.digital',
   WORKSPACE_PATH: '/wsdata', // path.resolve(__dirname, 'workspace'), // use In Local development
   MAX_FILE_SIZE: 10 * 1024 * 1024, // 10MB in bytes
 };
